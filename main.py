53b8aedc2afd28839a5f35708b7c170381aa6a7a
Ben Brittain <ben@brittain.org>
2015-11-22 18:21:41 -0500

    here ya go

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ from io import StringIO, BytesIO
 from collections import Counter
 
 parser = etree.XMLParser(ns_clean=True, recover=True)
-tree   = etree.parse('jun17-p2.xml', parser)
+tree   = etree.parse('jun14-p1.xml', parser)
 root = tree.getroot()
 
-ipdict = Counter()
+ipdict = dict()
 
 traits = np.array([[0, 0]])
 classification = np.array([[1]])
 for child in root:
-    if child.find('appName').text == "SSH":
+    if child.find('appName').text == "HTTPWeb" and (child.find('destination').text[:len('192.168.5.')] == '192.168.5.'):
         source = child.find('source').text
         dest = child.find('destination').text
         is_malicious = child.find('Tag').text == 'Attack'
@@ -27,58 +27,58 @@ for child in root:
         end_date = datetime.strptime(end, '%Y-%m-%dT%H:%M:%S')
         duration = (end_date - start_date).total_seconds()
         total_bytes = int(child.find('totalSourceBytes').text) + int(child.find('totalDestinationBytes').text)
-        ipdict[source] += 1
-        traits = np.append(traits, [[duration, ipdict[source]]], axis=0)
-        if is_malicious:
-            classification = np.append(classification, [1])
-        else:
-            classification = np.append(classification, [0])
+        #if source not in ipdict:
+        #    ipdict[source] = dict()
+        #    ipdict[source]['duration'] = 0
+        #    ipdict[source]['total_bytes'] = 0
+        #    ipdict[source]['count'] = 0
+        malicious = 1 if is_malicious else 0
+        #print(is_malicious)
+        #ipdict[source]['duration'] += duration
+        #ipdict[source]['total_bytes'] += total_bytes
+        #ipdict[source]['count'] += 1
+        traits = np.append(traits, [[duration, total_bytes/1024]], axis=0)
+        classification = np.append(classification, [malicious])
 
-h = .02  # step size in the mesh
 
-print(len(classification))
-print(len(traits))
+print(traits)
+print(classification)
+#print(ipdict.keys())
+#for source in ipdict.values():
+#    print(source)
+
+h = 1.0  # step size in the mesh
+
 C = 1.0  # SVM regularization parameter
-svc = svm.SVC(kernel='linear', C=C).fit(traits, classification)
-print("doing an svm")
-#rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(traits, classification)
-#print("doing an svm")
-#poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(traits, classification)
-#print("doing an svm")
-#lin_svc = svm.LinearSVC(C=C).fit(traits, classification)
-#print("doing an svm")
 
-# create a mesh to plot in
 x_min, x_max = traits[:, 0].min() - 1, traits[:, 0].max() + 1
+print(x_min, x_max)
 y_min, y_max = traits[:, 1].min() - 1, traits[:, 1].max() + 1
+print(y_min, y_max)
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+print(xx, yy)
 
-# title for the plots
-titles = ['SVC with linear kernel',
-        'LinearSVC (linear kernel)',
-        'SVC with RBF kernel',
-        'SVC with polynomial (degree 3) kernel']
+print("fitting svm")
+#clf = svm.SVC(kernel='linear', C=C).fit(traits, classification)
+clf = svm.SVC(kernel='rbf', gamma=0.3, C=C).fit(traits, classification)
+#clf = svm.SVC(kernel='poly', degree=3, C=C).fit(traits, classification)
+print("predicting")
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
 
-for i, clf in enumerate((svc, svc)):
-    # Plot the decision boundary. For that, we will assign a color to each
-    # point in the mesh [x_min, m_max]x[y_min, y_max].
-    plt.subplot(2, 2, i + 1)
-    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+## Put the result into a color plot
+print("reshaping")
+Z = Z.reshape(xx.shape)
 
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+print("drawing")
+plt.title("Malicious?")
+plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
+plt.xlabel('duration of connection')
+plt.ylabel('bytes transfered (kb)')
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+plt.xticks(())
+plt.yticks(())
 
-    # Put the result into a color plot
-    Z = Z.reshape(xx.shape)
-    plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
-
-    # Plot also the training points
-    plt.scatter(traits[:, 0], traits[:, 1], c=classification, cmap=plt.cm.Paired)
-    plt.xlabel('duration of connection')
-    plt.ylabel('connection count')
-    plt.xlim(xx.min(), xx.max())
-    plt.ylim(yy.min(), yy.max())
-    plt.xticks(())
-    plt.yticks(())
-    plt.title(titles[i])
+b = plt.scatter(traits[:, 0], traits[:, 1], c=classification, cmap=plt.cm.Paired)
 
 plt.show()
